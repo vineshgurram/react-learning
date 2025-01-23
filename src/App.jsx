cc763019f11a67915e757645915973dd51148c79
Vinesh <vineshgurram@gmail.com>
2025-01-23 08:29:52 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import Students from "./components/Students"
-import Fruits from "./components/Fruits"
-import Hello from "./components/Hello"
-import ConditionalComponents from "./components/CondtionalComponents"
-import Message from "./components/Message"
-import Counter from "./components/Counter"
-import Form from "./components/Form"
-import Forms from "./components/Forms"
+import Students from "./components/Students";
+import Fruits from "./components/Fruits";
+import Hello from "./components/Hello";
+import ConditionalComponents from "./components/CondtionalComponents";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import Form from "./components/Form";
+import Forms from "./components/Forms";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
+import Nav from "./components/Nav";
 
 function App() {
   const person = {
     name: "Vinesh Gurram",
     rollNo: 21,
-    greetings: "Good Morning"
-  }
+    greetings: "Good Morning",
+  };
   return (
-    <>
-      <div className="App">
-        {/* <Hello person={person} /> */}
-        {/* <Fruits /> */}
-        {/* <Students /> */}
-        {/* <ConditionalComponents /> */}
-        {/* <Message /> */}
-        {/* <Counter /> */}
-        {/* <Form /> */}
-        <Forms />
-      </div>
-    </>
-  )
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
